b40e9bfb82b4d245411d45b499973879b0db071e
taqiudeen275 <55208368+taqiudeen275@users.noreply.github.com>
2024-05-16 10:12:47 +0000

    sales stage development

--- lib/screen/sales.dart
+++ lib/screen/sales.dart
@@ -23,64 +23,97 @@ class _SalesScreenState extends State<SalesScreen> {
   Widget build(BuildContext context) {
     return Obx(() => Column(
           children: [
-            Text(productController.selectedItems.length.toString()),
-            FilledButton(
-                onPressed: () {
-                  bigActionModal(context, const Text("Add Sale"), OrderPick(), [
-                    FilledButton(
-                        child: const Text("Cancel"),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        }),
-                    Obx(() => Row(
-                          children: [
-                            const Text("Credit Sale"),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            ToggleSwitch(
-                              checked: isCredit.value,
-                              onChanged: productController.selectedItems.isEmpty
-                                  ? null
-                                  : (bool value) {
-                                      isCredit.value = value;
-                                    },
-                            ),
-                            const SizedBox(
-                              width: 20,
-                            ),
-                            Expanded(
-                              child: FilledButton(
-                                  onPressed: productController
-                                          .selectedItems.isEmpty
-                                      ? null
-                                      : () async {
-                                          Sale saleAdded = await salesController
-                                              .addGetModel(Sale(
-                                                  isCredit: isCredit.value,
-                                                  date: DateTime.now(),
-                                                  
-                                                  amount: orderController
-                                                      .selectedItems
-                                                      .fold(
-                                                          0.0,
-                                                          (sum, product) =>
-                                                              sum! +
-                                                              (product.amount! *
-                                                                  product
-                                                                      .quantity!))));
-                                          await orderController
-                                              .onItemSelectedSave(saleAdded);
+            Text(orderController.models.length.toString()),
+            SizedBox(
+              height: MediaQuery.of(context).size.height*0.1,
 
-                                        },
-                                  child: const Text("Record Sale")),
-                            ),
-                          ],
-                        )),
-                  ]);
-                },
-                child: const Text("Select Product"))
+              child: RecordSale(
+                  isCredit: isCredit,
+                  productController: productController,
+                  salesController: salesController,
+                  orderController: orderController),
+            ),
+            ...salesController.models.map((element) =>  Expander(
+                  header: Text('SalesID ${element.id.toString()}'),
+                  content: SizedBox(
+                    height: 300,
+                    child: SingleChildScrollView(
+                      child: Text('Amount ${element.amount.toString()}'),
+                    ),
+                  ),
+                ))
           ],
         ));
+  }
+}
+
+class RecordSale extends StatelessWidget {
+  const RecordSale({
+    super.key,
+    required this.isCredit,
+    required this.productController,
+    required this.salesController,
+    required this.orderController,
+  });
+
+  final RxBool isCredit;
+  final ProductController productController;
+  final SaleController salesController;
+  final OrderController orderController;
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton(
+        onPressed: () {
+          bigActionModal(context, const Text("Add Sale"), OrderPick(), [
+            FilledButton(
+                child: const Text("Cancel"),
+                onPressed: () {
+                  Navigator.pop(context);
+                }),
+            Obx(() => Row(
+                  children: [
+                    const Text("Credit Sale"),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    ToggleSwitch(
+                      checked: isCredit.value,
+                      onChanged: productController.selectedItems.isEmpty
+                          ? null
+                          : (bool value) {
+                              isCredit.value = value;
+                            },
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                      child: FilledButton(
+                          onPressed: productController.selectedItems.isEmpty
+                              ? null
+                              : () async {
+                                  
+                                      await salesController.addModel(Sale(
+                                          isCredit: isCredit.value,
+                                          date: DateTime.now(),
+                                          amount: orderController.selectedItems
+                                              .fold(
+                                                  0.0,
+                                                  (sum, product) =>
+                                                      sum! +
+                                                      (product.amount! *
+                                                          product.quantity!))));
+                                  await orderController
+                                      .onItemSelectedSave(salesController.models.last);
+                                  Navigator.pop(context);
+                                },
+                          child: const Text("Record Sale")),
+                    ),
+                  ],
+                )),
+          ]);
+        },
+        child: const Text("Select Product"));
   }
 }
